7c7271d4c5ba6b7992d8be7669968c67f3dc0261
Aarav Shah <74358472+Aaravshah2907@users.noreply.github.com>
2026-06-29 13:02:43 +0530

    Release v2.2.2

--- setup.sh
+++ setup.sh
@@ -172,7 +172,7 @@ def as_int(value, default, minimum=0):
 
 config = {
     "config_version": 1,
-    "app_version": "2.2.1",
+    "app_version": "2.2.2",
     "default_language": sys.argv[2] or "cpp",
     "display": {
         "theme": sys.argv[5] or "textual-dark",
