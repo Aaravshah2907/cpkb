e88cd9ae7ca073877906462d4892f68da966edaf
Aarav Shah <74358472+Aaravshah2907@users.noreply.github.com>
2026-09-16 20:09:15 +0530

    chore(formula): update cpkb.rb sha256 for v3.0.0 release

--- Formula/cpkb.rb
+++ Formula/cpkb.rb
@@ -2,7 +2,7 @@ class Cpkb < Formula
   desc "Competitive Programming Knowledge Base (High-Performance Rust Edition)"
   homepage "https://github.com/Aaravshah2907/cpkb"
   url "https://github.com/Aaravshah2907/cpkb/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "f6e77c8cbb00132f1ddaaa7e11492ec4dbc3808e11f33f096444f95c4ae04b02"
   license "MIT"
 
   depends_on "rust" => :build
